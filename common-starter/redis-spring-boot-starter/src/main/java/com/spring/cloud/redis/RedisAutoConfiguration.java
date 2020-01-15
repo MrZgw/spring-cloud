34ee5f048cbe482e370992144972efa0b27f46fd
zgw <675559200@qq.com>
2020-01-15 18:14:41 +0800

    框架集成

---
+++ common-starter/redis-spring-boot-starter/src/main/java/com/spring/cloud/redis/RedisAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.redis;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName RedisAutoConfiguration
+ * @Description redis配置类
+ * @Author zgw
+ * @Date 2020/1/15 18:11
+ **/
+@Configuration
+public class RedisAutoConfiguration {
+}

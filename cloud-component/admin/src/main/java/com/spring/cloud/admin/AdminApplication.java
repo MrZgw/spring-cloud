364092f299cee87cc9a8c86b00225392469ff0b3
zhanguowei <15001397928@163.com>
2020-04-17 15:30:16 +0800

    config commit

---
+++ cloud-component/admin/src/main/java/com/spring/cloud/admin/AdminApplication.java
@@ -0,0 +1,23 @@
+package com.spring.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName AdminApplication
+ * @Description 启动类型
+ * @Author zhanguowei
+ * @Date 2020/4/16 16:31
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
